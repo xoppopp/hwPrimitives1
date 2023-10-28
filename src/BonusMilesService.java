7ac0f42c632c436ff5c6e7a8441b96e4e5933398
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-10-28 16:46:32 +0300

    method added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int result;
+        result = price / 20;
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 12_345; // Стоимость билета
-        int mile = 20; // Рублей за милю
-        int finaL = ticket / mile; // Итоговый результат
-        System.out.println((("При стоимости билета ") + ticket) + " рублей");
-        System.out.println((("Начисляется ") + finaL) + " Миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 12_345;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
