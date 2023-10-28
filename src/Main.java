ea5f6f30bc918c386ba1d18154373daf16e90fb6
Nikolay Khlyupikov <xoppopp13@gmail.com>
2023-10-28 12:16:24 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 12_345; // Стоимость билета
+        int mile = 20; // Рублей за милю
+        int finaL = ticket / mile; // Итоговый результат
+        System.out.println((("При стоимости билета ") + ticket) + " рублей");
+        System.out.println((("Начисляется ") + finaL) + " Миль");
+    }
+}
